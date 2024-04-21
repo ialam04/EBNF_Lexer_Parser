e907aa9df3b6ce44b53339d210884570c2c80fca
ialam04 <ialam2@lsu.edu>
2024-04-21 18:52:50 -0500

    l

--- LexerParser/obj/Debug/net8.0/LexerParser.AssemblyInfo.cs
+++ LexerParser/obj/Debug/net8.0/LexerParser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LexerParser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3835d74c79a2399bdb685b2d5418db13a9867192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LexerParser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LexerParser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
